0c389fbfea9473100f015f54003bf676d5878492
zacaffinito <zaffinito@icloud.com>
2023-12-13 14:11:34 -0800

    progress3

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 # Create a dictionary to store the daily calorie intake
 import pygame
 import sys
+import calendar
 
 calorie_tracker = {}
+
+
 
 def display_calories_window(date):
     pygame.init()
@@ -25,6 +28,7 @@ def display_calories_window(date):
         pygame.display.update()
 
     pygame.quit()
+
 
 
 def add_calories(date, calories):
@@ -55,4 +59,95 @@ def draw_text(text, x, y, color=black):
     text_surface = font.render(text, True, color)
     window.blit(text_surface, (x, y))
 
+def main():
+    running = True
+    while running:
+        window.fill(white)
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    input_date = True
+                    input_text = ""
+                    while input_date:
+                        for event in pygame.event.get():
+                            if event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_RETURN:
+                                    input_date = False
+                                elif event.key == pygame.K_BACKSPACE:
+                                    input_text = input_text[:-1]
+                                else:
+                                    input_text += event.unicode
+
+                        window.fill(white)
+                        draw_text("Enter date (MM-DD-YY):", 50, 50)
+                        draw_text(input_text, 50, 100)
+                        pygame.display.update()
+
+                    date = input_text
+                    input_text = ""
+                    input_calories = True
+                    while input_calories:
+                        for event in pygame.event.get():
+                            if event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_RETURN:
+                                    input_calories = False
+                                elif event.key == pygame.K_BACKSPACE:
+                                    input_text = input_text[:-1]
+                                else:
+                                    input_text += event.unicode
+
+                        window.fill(white)
+                        draw_text("Enter calories:", 50, 50)
+                        draw_text(input_text, 50, 100)
+                        pygame.display.update()
+
+                    calories = int(input_text)
+                    add_calories(date, calories)
+
+                elif event.key == pygame.K_d:  # Press 'D' to view past dates' calorie info
+                    input_date = True
+                    input_text = ""
+                    while input_date:
+                        for event in pygame.event.get():
+                            if event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_RETURN:
+                                    input_date = False
+                                elif event.key == pygame.K_BACKSPACE:
+                                    input_text = input_text[:-1]
+                                else:
+                                    input_text += event.unicode
+
+                        window.fill(white)
+                        draw_text("Enter date (MM-DD-YY):", 50, 50)
+                        draw_text(input_text, 50, 100)
+                        pygame.display.update()
+
+                    date = input_text
+
+                    # Display calories for the entered date
+                    text = display_calories(date)
+                    draw_text(text, 50, 150)
+
+
+
+        instructions = [
+            "Welcome to the Calorie Tracker!",
+            "Press 'A' to add calories.",
+            "Press 'D' to view past calorie entries", 
+            "Follow the prompts on the screen to enter date and calories.",
+        ]
+        for i, line in enumerate(instructions):
+            draw_text(line, 50, 50 + i * 30, black)
+
+        text = display_calories("2023-01-01")  # Replace "2023-01-01" with the date you want to display
+        draw_text(text, 50, 200, black)
+
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
