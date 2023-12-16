26c6b948c2209883a80d5d78610d337afa5cf71f
zacaffinito <zaffinito@icloud.com>
2023-12-16 13:07:42 -0800

    progressfinal

---
+++ Affinito_Zac_FINAL/ideation.py
@@ -0,0 +1,7 @@
+# File by Zac Affinito
+
+# What is something you're passionate about or interested in?  
+# How can you apply python and its many libraries towards that interest in your final project?
+
+# Calorie Tracker
+# essentially take info and output specific information based on that info
--- ideation.py
+++
@@ -1,9 +0,0 @@
-# File by Zac Affinito
-
-# What is something you're passionate about or interested in?  
-# How can you apply python and its many libraries towards that interest in your final project?
-
-# Calorie Tracker
-# user inputs height and weight, selects bulking vs. cutting, creates calorie plan
-# maybe give a detailed plan to weight loss for specific goals that the user intends
-# essentially take info and output specific information based on that info
